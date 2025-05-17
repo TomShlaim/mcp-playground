f504084869abb5097c62c6df961c367dbe2eb59e
TomShlaim <tom@zyg.com>
2025-05-17 12:27:41 +0300

    Added logic

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from mcp-playground!")
+from mcp.server.fastmcp import FastMCP
 
+mcp = FastMCP("food")
+
+def make_jasmino_request(serving = "Pita Kebab") -> str:
+    #go to jasmino
+    #Wait the line
+    #order
+    #pay
+    #get the receipt
+    #return the receipt
+
+    return f"You got your {serving} at jasmino"
+
+def make_rambam_request(serving = "Pita Shwarma") -> str:
+    #go to rambam
+    #Wait the line
+    #order
+    #pay
+    #get the receipt
+    #return the receipt
+
+    return f"You got your {serving} at rambam"
+
+@mcp.tool(description="Order a specific food item from Jasmino and receive a receipt.")
+async def get_jasmino(serving : str) -> str:
+    return make_jasmino_request(serving)
+
+@mcp.tool(description="Order a specific food item from Rambam and receive a receipt.")
+async def get_rambam(serving: str) -> str:
+    return make_rambam_request(serving)
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport='stdio')
